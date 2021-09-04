f3190381c059a64927e6ac7ff8f4d6779960ad57
Dima Zeklam <dema.ziklam@gmail.com>
2021-09-04 10:15:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
 import Header from './Header';
 import Footer from './Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './Profile';
+import BsetUni from './BsetUni';
+import Login from './Login';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Favorite from './Favorite';
+
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    }
-  }
-
-  loginHandler = (user) => {
-    this.setState({
-      user,
-    })
-  }
-
-  logoutHandler = () => {
-    this.setState({
-      user: null,
-    })
-  }
 
   render() {
+    const { isAuthenticated } = this.props.auth0;
+    // console.log('app', this.props);
     return (
       <>
         <Router>
-          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          {/* <IsLoadingAndError> */}
+          <Header />
           <Switch>
             <Route exact path="/">
               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-            </Route>
-            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              {(isAuthenticated ? <BsetUni /> : <Login />)}
+
+            </Route >
+            <Route exact path="/profile">
+              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Profile />
+            </Route >
+            <Route exact path="/favorite">
+            {isAuthenticated && (<Favorite />)}
+
+
+            </Route >
+
           </Switch>
           <Footer />
         </Router>
       </>
-    )
+    );
   }
 }
 
-export default App;
+export default withAuth0(App);
---
+++ src/BsetUni.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { withAuth0 } from "@auth0/auth0-react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button,Card } from 'react-bootstrap';
+import axios from 'axios';
+// import ShowUni from './ShowUni';
+
+
+class BsetUni extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      universities: []
+
+    }
+  }
+
+  // server.get('/getApi', getApi);
+  // server.post('/addFav', addFav);
+  /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  //----------- Get from API ---------
+  getApi = async (e) => {
+    e.preventDefault();
+    let city = e.target.country.value;
+    console.log('cityName: ', city);
+    let url = `${process.env.REACT_APP_SERVER}/getApi?country=${city}`;
+    let UniData = await axios.get(url);
+    // console.log("UniData:", UniData.data);
+    this.setState({
+      universities: UniData.data
+    })
+    console.log("universities:", this.state.universities);
+  }
+
+  // ------------- Add To Favourit -------------
+  addFav = async (ele) => {
+    const { user } = this.props.auth0;
+    let uniData = {
+      country: ele.country,
+      name: ele.name,
+      web_pages: ele.web_pages,
+      email: user.email
+    }
+    console.log('uniData', uniData);
+    let url = `${process.env.REACT_APP_SERVER}/addFav`;
+    let FavData = await axios.post(url, uniData);
+    console.log('FavData:', FavData.data)
+    // this.setState({
+    //   favourit: FavData.data
+    // })
+  }
+
+  render() {
+
+    /* TODO: render user's books in a Carousel */
+
+    return (
+      <>
+        <Form onSubmit={this.getApi}>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>country</Form.Label>
+            <Form.Control type="text" placeholder="Jordan" name="country" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Search
+          </Button>
+        </Form>
+
+        {this.state.universities.length ? (
+          this.state.universities.map((item, key) => {
+            return (
+
+              <Card key={key} style={{ width: '18rem' }}>
+
+                <Card.Body>
+                  <Card.Text>
+                    {item.country}
+                  </Card.Text>
+                  <Card.Text>
+                    {item.name}
+                  </Card.Text>
+                  <Card.Text>
+                    <a href={item.web_pages}>Web Page</a>
+                  </Card.Text>
+                  <Button onClick={()=>this.addFav(item)} variant="primary">Add to favourit</Button>
+                </Card.Body>
+              </Card>
+
+            );
+          })
+
+
+        ) : (
+          <h3>No Books Found :(</h3>
+        )}
+      </>
+    )
+  }
+}
+
+export default withAuth0(BsetUni);
